5ebf4df245ad92ac36ca9d77d4e8933f57d8b232
zonkisa <271169495@qq.com>
2019-05-30 22:50:30 +0800

    add improved Insertion.sort, Shell.sort and sortCompare class

--- TheAlgorithms/data_structures/python/sorts/sort_models.py
+++ TheAlgorithms/data_structures/python/sorts/sort_models.py
@@ -42,10 +42,88 @@ class Insertion(Example):
         for i in range(1, N):
             j = i
             while j > 0 and self.less(collection[j], collection[j-1]):
-                j -= 1
                 self.exch(collection, j, j-1)
+                j -= 1
 
         return collection
 
+    """
+    插入排序对如下部分有序的数组很有效（倒置数小于数组大小某个倍数为部分有序）：
+    1，数组中每个元素距离它最终位置较近
+    2，一个有序的大数组接一个小数组
+    3，数组中只有几个元素的位置不正确
+    
+    上述三种情况都说明同一个条件，数组中需要倒置的数量较少。
+    
+    插入排序需要的交换操作和数组中倒置的数量相同，
+    需要比较的次数大于等于倒置的数量，小于等于倒置的数量+N-1
+    注意： EXAMPLE 有11对倒置
+    """
+
+    # 改进1，减少交换次数  --> 算法第四版习题 2.1.25
+    # 这里实际是拿当前值与左边各个索引值做比较，若小于，则将左侧值大的索引+1
+    # 元素实际移动路径与原插入排序无区别，但由于tmp临时变量，减少了数组的访问次数
+    def sort2(self, collection):
+        N = len(collection)
+        for i in range(1, N):
+            tmp = collection[i]
+            j = i
+            while j > 0 and self.less(tmp, collection[j-1]):
+                collection[j] = collection[j-1]
+                j -= 1
+            collection[j] = tmp
+
+        return collection
+
+    # 改进2，规避边界测试，插入排序哨兵 --> 算法第四版习题 2.1.24
+    def sort3(self, collection):
+        N = len(collection)
+
+        min_index = 0
+        for i in range(1, N):
+            if self.less(collection[i], collection[min_index]):
+                min_index = i
+        self.exch(collection, min_index, 0)
+
+        for i in range(2, N):
+            tmp = collection[i]
+            j = i
+            while self.less(tmp, collection[j - 1]): # 此时无需判断j>0
+                collection[j] = collection[j - 1]
+                j -= 1
+            collection[j] = tmp
+
+        return collection
+
+
+class Shell(Example):
+    """
+    前面已经提到，插入排序对部分有序的数组是比较有效的
+    希尔排序的思想是使数组中任意间隔为h的元素都是有效的（h有效数组）
+    希尔排序由于权衡了子数组的规模和有序性。
+
+    官方例子中规模因子为3，关于局部有序性如下：（注意这里的局部有序并不严谨，仅为加深理解）
+    由于规模因子为3，初始h会接近N/3附近。
+    先做极端假设的实例，恰好初始h的左侧均大于右侧部分，此时假设初始三部分数据形如[大, 小, 未知]
+    这样第一层外循环在走位"大"部分的数据后，会使得初始h索引两侧的约N/3数据相对有序。即会倾向于[小, 大, 未知]
+    第一层外循环结束后，h = h/3，此时再关注"小"部分数据，几乎又会重复上述过程。
+    经过类似过程完成数组的部分有序
+
+    希尔排序时间复杂度：O(n^(1.3—2))，官方建议中等大小数组可临时使用
+    """
+    def sort(self, collection):
+        N = len(collection)
+        h = 1
+        while h < N/3:
+            h = 3 * h + 1
+        while h >= 1:
+            for i in range(h, N):
+                j = i
+                while j >= h and self.less(collection[j], collection[j - h]):
+                    self.exch(collection, j, j-h)
+                    j -= h
+            h = int(h/3)
+
+        return collection
 
 
--- TheAlgorithms/data_structures/python/sorts/sort_test.py
+++ TheAlgorithms/data_structures/python/sorts/sort_test.py
@@ -1,9 +1,60 @@
 # coding:utf-8
 from __future__ import print_function
-from TheAlgorithms.data_structures.python.sorts.sort_models import Selection
+from TheAlgorithms.data_structures.python.sorts.sort_models import Selection, Insertion, Shell
+from time import perf_counter_ns
+from random import randint
+import math
+
+
+class SortCompare:
+
+    @staticmethod
+    def algTime(alg, collection):
+        start = perf_counter_ns()/1000 # 微秒
+        if alg == "Insertion":
+            Insertion().sort(collection)
+        if alg == "Selection":
+            Selection().sort(collection)
+        if alg == "Shell":
+            Shell().sort(collection)
+        # if alg == "Merge":
+        #     Merge().sort(collection)
+        # if alg == "Quick":
+        #     Quick().sort(collection)
+        # if alg == "Heap":
+        #     Heap().sort(collection)
+        return perf_counter_ns()/1000 - start
+
+    def timeRandomInput(self, alg, N, T):
+        total = 0.0
+        collection = [0] * N
+        for i in range(T):
+            for j in range(N):
+                collection[j] = randint(0, N-1)
+            total += self.algTime(alg, collection)
+
+        return total
+
+    def cmpRes(self, alg1, alg2, N, T):
+        t1 = self.timeRandomInput(alg1, N, T)
+        t2 = self.timeRandomInput(alg2, N, T)
+        print("{} is {} times faster than {}".format(alg1, round(t2/t1, 2), alg2))
+
+
+def p(x, y):
+    return int(math.pow(x, y))
 
 
 if __name__ == '__main__':
-    a = Selection()
+    t = [5, 4, 3, 2, 7, 6, 1]
+    print(Selection().sort(t))
+    print(Insertion().sort(t))
+    print(Insertion().sort2(t))
+    print(Insertion().sort3(t))
+    # SortCompare().cmpRes("Insertion", "Selection", 1000, 100)
+    print(SortCompare().timeRandomInput("Shell", p(2, 7), 10))
 
-    print(a.sort([5, 4, 3, 2, 7, 6, 1]))
+    print(SortCompare().timeRandomInput("Shell", p(2, 8), 10))
+    print(SortCompare().timeRandomInput("Shell", p(2, 9), 10))
+    print(SortCompare().timeRandomInput("Shell", p(2, 10), 10))
+
